663d79052b085d55b78b3459dfeb404102495d43
Omer <OZadok@gmail.com>
2019-01-25 00:01:11 +0200

    add PlayableCharacter and controller

---
+++ Assets/Scripts/PlayableCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableCharacter : MonoBehaviour
+{
+
+    [SerializeField][Range(0,100)] private int stress;
+    [Range(0,100)] private int stressMinThresholds;
+
+    public void ApplyStressImpact(int stressImpact)
+    {
+        stress += stressImpact; 
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
