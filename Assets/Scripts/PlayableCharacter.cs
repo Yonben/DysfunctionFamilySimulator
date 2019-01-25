899b3d2ef86f966df5b62e9df933c07d5512b047
Omer <OZadok@gmail.com>
2019-01-25 18:28:37 +0200

    Failliure condition

--- Assets/Scripts/PlayableCharacter.cs
+++ Assets/Scripts/PlayableCharacter.cs
@@ -1,17 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
+[RequireComponent(typeof(PlayerController))]
 public class PlayableCharacter : MonoBehaviour
 {
 
     [SerializeField] [Range(0, 100)] private int stress;
     [Range(0, 100)] private int stressMinThresholds;
-    public PlayerController PlayerController;
+    [SerializeField] private int maxStress = 100; 
+    private PlayerController PlayerController;
+
+    private void Awake()
+    {
+        PlayerController = GetComponent<PlayerController>();
+    }
 
     public void ApplyStressImpact(int stressImpact)
     {
         stress += stressImpact;
+        stress = Mathf.Max(stress, stressMinThresholds);
         print("ApplyStressImpact");
+
+        if (stress >= maxStress)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //lock stress??
+        
+        //remove the player visually with animation
+        //todo
+        
+        // remove player control.
+        enabledControl(false);
+        
+        //die on GameManager
+        GameManager.instance.PlayerDie(this);
+    }
+
+    public void enabledMovement(bool enable=true)
+    {
+        PlayerController.canMove = enable;
+    }
+    
+    private void enabledControl(bool enable=true)
+    {
+        KeyValuePair<int, int> valuePair = new KeyValuePair<int, int>(1,1);
+        enabledMovement(enable);
+        //todo - also enable buttons.
     }
 }
