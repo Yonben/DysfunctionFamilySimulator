59006ff7aa9698b12d2f61bf40b5a3c5a770a6e3
Yonathan Benolol <yonathan@10bis.co.il>
2019-01-25 11:25:33 +0200

    Added AllButtons minigame

---
+++ Assets/Scripts/AllButtonsMiniGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllButtonsMiniGame : MiniGame
+{
+    public enum buttons { A, B, C, D };
+    public List<buttons> pressableButtons = new List<buttons> { buttons.A, buttons.B, buttons.C, buttons.D };
+    public int numberOfTap = 25;
+
+    void Start()
+    {
+
+    }
+
+    public override void PlayGame()
+    {
+        foreach (var button in pressableButtons)
+        {
+            // Check if pressed
+            if (true)
+            {
+                numberOfTap--; // Or -4?
+                if (numberOfTap <= 0)
+                {
+                    EndMiniGame();
+                }
+            }
+        }
+    }
+}
