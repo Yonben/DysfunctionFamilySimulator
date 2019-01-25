8ea9e80da65c47aa9ded360d8424cfcfe56ab1f6
blisf <35866788+blisf@users.noreply.github.com>
2019-01-25 19:57:39 +0200

    Tested sound system on PatternButton
    Added sound properties that are tied to the animation of the button

---
+++ Assets/soundExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundExample : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        
+    }
+    public void playSound()
+    {
+        audioSource.Play();
+        print("sound");
+    }
+}
