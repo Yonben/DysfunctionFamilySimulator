1fc3e38e19acf7f310588ae9c9c2972ee2f8e6fa
Omer <OZadok@gmail.com>
2019-01-26 13:00:44 +0200

    Dog Love Aura

---
+++ Assets/Scripts/LoveAura.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoveAura : MonoBehaviour
+{
+    [SerializeField] private int stressToReduce;
+
+    [SerializeField] private float timeToEachReducedStress;
+    
+    private List<PlayableCharacter> players = new List<PlayableCharacter>();
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayableCharacter player = other.GetComponent<PlayableCharacter>();
+        if (!player)
+            return;
+
+        players.Add(player);
+        StartCoroutine(nameof(ReduceStress), player);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        PlayableCharacter player = other.GetComponent<PlayableCharacter>();
+        if (!player)
+            return;
+        players.Remove(player);
+    }
+
+    IEnumerator ReduceStress(PlayableCharacter player)
+    {
+        yield return new WaitForSeconds(timeToEachReducedStress);
+        while (players.Contains(player))
+        {
+            player.ApplyStressImpact(stressToReduce);
+            yield return new WaitForSeconds(timeToEachReducedStress);
+        }
+    }
+}
