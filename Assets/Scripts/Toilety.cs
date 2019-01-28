b86558cdbe2c9430788befa54e51e9f21f0665ea
idanro <idan.rooze@gmail.com>
2019-01-28 21:55:58 +0200

    added toilet need

---
+++ Assets/Scripts/Toilety.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Toilety : IndependentPenaltyBehviour
+{
+    public float maxTimeToBeToilety;
+    public float minTimeToBeToilety;
+    [SerializeField] private ActionableObject _actionableObject;
+
+    private void Awake()
+    {
+        base.Awake();
+        ToiletTimer();
+    }
+
+    public override void Off()
+    {
+        base.Off();
+        ToiletTimer();
+    }
+
+    private void ToiletTimer()
+    {
+        Invoke("BecomeToilety", Random.Range(minTimeToBeToilety, maxTimeToBeToilety));
+    }
+
+    private void BecomeToilety()
+    {
+
+        print("Became Toilety");
+        IsOn = true;
+
+        _actionableObject.AddApplicableCharacter(player.PlayerType, this);
+        StartCoroutine(nameof(TakePenalty));
+    }
+}
