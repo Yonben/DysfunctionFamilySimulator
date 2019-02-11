2d0cdc2de1ffc46245513a6a9f57d1b0fa148712
Omer <OZadok@gmail.com>
2019-02-11 14:44:32 +0200

    Start Screen

---
+++ Assets/StartScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
+
+public class StartScreen : MonoBehaviour
+{
+    public SpriteRenderer ground;
+    public Sprite secondSprite;
+
+    private int state = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (XCI.GetButtonDown(XboxButton.A))
+        {
+            if (state == 0)
+            {
+                ground.sprite = secondSprite;
+            }
+            else
+            {
+                SceneManager.LoadScene("level");
+            }
+            state++; 
+        }
+    }
+}
